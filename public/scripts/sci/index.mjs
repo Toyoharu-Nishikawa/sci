6e3d0c9d3da95b6ebf44802c302f17619fbef3b1
toyoharu nishikawa <toyohal@email.com>
2018-11-18 19:09:59 +0900

    add basic statistics

--- public/scripts/sci/index.mjs
+++ public/scripts/sci/index.mjs
@@ -1,7 +1,8 @@
-import * as interpolate from "./interpolate/index.mjs"
-import * as solve from "./solve/index.mjs"
-import * as matrix from "./matrix/index.mjs"
-import * as optimize from "./optimize/index.mjs"
+import  * as interpolate from "./interpolate/index.mjs"
+import  * as solve from "./solve/index.mjs"
+import  * as matrix from "./matrix/index.mjs"
+import  * as optimize from "./optimize/index.mjs"
+import  * as statistics from "./statistics/index.mjs"
 
-export {interpolate, solve, matrix, optimize}
+export {interpolate, solve, matrix, optimize,statistics}
 
