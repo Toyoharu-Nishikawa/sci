d94dc1678cd7f21294eaae7a27f0b935e48b6e74
toyoharu nishikawa <toyohal@email.com>
2019-01-07 17:35:42 +0900

    add gauss kernel regression

---
+++ public/scripts/sci/regression/gkr.mjs
@@ -0,0 +1,20 @@
+import {modifiedCholeskyDecomposition, modifiedCholeskySolve} from "../solve/linerEquation.mjs"
+
+import {makeGaussKernel} from "../funcs/index.mjs"
+
+"use strict"
+
+export const gaussKernelRegression = (x,y,beta,C)=>{
+  const kernel = makeGaussKernel(beta)
+  const originK = x.map((v,i,arr)=>[].concat(
+    [...Array(i)].map((u,j)=>kernel(v,arr[j])),
+    1+1/C
+  ))
+  const LDLT = modifiedCholeskyDecomposition(originK)  
+  const alpha = modifiedCholeskySolve(LDLT.L, LDLT.D, y)
+  const f = (t)=> alpha.map((v,i)=>v*kernel(x[i],t)).reduce((p,c)=>p+c,0)
+  return {
+    predict: f,
+    parameters:{alpha: alpha, beta: beta, x:x, y:y}
+  } 
+}
