2bec571a099f27bd5f23de6059df971a240b202d
toyoharu nishikawa <toyohal@email.com>
2020-05-23 00:01:07 +0900

    added capability to calculate derivative  of bspline and nurbs curve

--- public/scripts/sci/interpolate/bspline.mjs
+++ public/scripts/sci/interpolate/bspline.mjs
@@ -1,8 +1,24 @@
 ﻿import * as solve from "../solve/index.mjs"
 
-const search = (knots,order, x)=>{
+const search = (knots, degree, x)=>{
+  const order = degree + 1
   const index= knots.slice(0, -order)
     .reduce((pre,current,index)=>current <= x ? index:pre, 0)
+  return index
+}
+
+const quickSearch = (knots, degree, x) => {
+  const order = degree + 1
+  const cand = knots.slice(0, -order)
+  let index = 0
+  for(let i=0;i<cand.length;i++){
+    if(cand[i] <=x){
+      index= i
+    }
+    else{
+      break
+    }
+  }
   return index
 }
 
@@ -23,13 +39,64 @@ const updateN = (N, knots, x, m, i)=>{
 }
 
 // return vector whose length is equal to knots.length - order
-export const makeN = (knots, order, x) => {
-  const index = search(knots, order, x)
+export const makeN = (knots, degree, x) => {
+  const order = degree + 1
+  const index = search(knots, degree, x)
   const num = knots.length - order
   const N1 =[...Array(num)].map((v,i)=>i===index?1:0)
   const N = [...Array(order-1)].reduce((pre,current,m)=>{
     return pre.map((v,i,arr)=>updateN(arr, knots, x, m+2,i))
   },N1)
+  return N
+}
+
+export const makeNmatrix = (knots, degree, x) => {
+  const order = degree + 1
+  const i = quickSearch(knots, degree, x)
+  const u = knots 
+  const m = u.length -order
+  
+  const Ntensor = [[[1]]]
+  for(let p=1;p<=degree;p++){
+    const NpMatrix = []
+    for(let k=0;k<=p;k++){
+      const list =[]  
+      for(let j=0;j<=p;j++){
+        const n= i-p+j
+        if(k==0){
+          const N1 = j==0 ? 0 : Ntensor[p-1][0][j-1]
+          const N2 = j==p ? 0 : Ntensor[p-1][0][j]
+          const c1 = u[n+p]-u[n] >0 ?  (x -u[n])/(u[n+p]-u[n])*N1 : 0
+          const c2 = u[n+p+1]-u[n+1] > 0 ? (u[n+p+1]-x)/(u[n+p+1]-u[n+1])*N2 : 0
+          const Nip = c1 + c2 
+
+          list.push(Nip)
+        } 
+        else{
+          const N1 = j==0 ? 0 : Ntensor[p-1][k-1][j-1]
+          const N2 = j==p ? 0 : Ntensor[p-1][k-1][j]
+          const c1 = u[n+p]-u[n] > 0 ? p/(u[n+p]-u[n])*N1 : 0
+          const c2 = u[n+p+1]-u[n+1] > 0 ? p/(u[n+p+1]-u[n+1])*N2 : 0
+          const Nip = c1 - c2 
+          list.push(Nip)
+        }
+      }
+      NpMatrix.push(list)
+    }
+    Ntensor.push(NpMatrix)      
+  }
+  
+
+  const Nmatrix = Ntensor[Ntensor.length-1]
+
+  //zero padding left and right
+  const N = Nmatrix.map(v=>{
+    const prefix = i-degree>0 ? [...Array(i-degree)].fill(0): []
+    const suffix = m-1-i>0 ? [...Array(m-1-i)].fill(0) :[] 
+    const list = [].concat(prefix, v, suffix)
+    return list
+  }) 
+
   return N
 }
 
@@ -58,11 +125,12 @@ export const bspline = (x, y, degree=3, k)=>{
   catch(e){
    console.log(e.name +" : " + e.message)
   }
-  const A = x.map((value, i) => makeN(knots, order, value))
+  const A = x.map((value, i) => makeNmatrix(knots, degree, value)[0])
   const c = solve.linEqGauss(A, y)
   const num = x.length 
-  return (x0)=>{ 
-    const N = makeN(knots, order, x0)
+  return (x0, k=0)=>{ 
+    const Nmatrix = makeNmatrix(knots, degree, x0)
+    const N = Nmatrix[k]
     const y0 = N.reduce((pre, current, i)=>pre+current*c[i],0)
     return y0
   }
